2c1b891ba6aac4867a0889232fcda52d8985a78f
Aaditya Mohite <mohiteaadi10@gmail.com>
2026-08-13 23:53:45 +0530

    Added Better aprroach

--- Array/LongestSubarray.java
+++ Array/LongestSubarray.java
@@ -1,25 +1,62 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class LongestSubarray {
+    // Brute Force approch
+    // public static int Longest(int[] arr, int k) {
 
-    public static int Longest(int[] arr, int k) {
+    // int maxlength = 0;
+    // for (int i = 0; i < arr.length; i++) {
 
+    // int sum = 0;
+
+    // for (int j = i; j < arr.length; j++) {
+    // sum += arr[j];
+
+    // if (sum == k) {
+    // int length = j - i + 1;
+    // maxlength = Math.max(maxlength, length);
+    // }
+
+    // }
+
+    // }
+    // return maxlength;
+    // }
+
+
+
+
+    
+    // Better approach
+
+    public static int Longest2(int[] arr, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int length = 0;
         int maxlength = 0;
+
         for (int i = 0; i < arr.length; i++) {
 
-            int sum = 0;
+            sum += arr[i];
 
-            for (int j = i; j < arr.length; j++) {
-                sum += arr[j];
+            if (sum == k) {
+                length = i + 1;
+            }
 
-                if (sum == k) {
-                    int length = j - i + 1;
-                    maxlength = Math.max(maxlength, length);
-                }
+            if (map.containsKey(sum - k)) {
+                length = i - map.get(sum - k);
+                maxlength = Math.max(maxlength, length);
+            }
 
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
             }
 
         }
+
         return maxlength;
     }
 
@@ -39,7 +76,7 @@ public class LongestSubarray {
 
         int k = sc.nextInt();
 
-        int result = Longest(arr, k);
+        int result = Longest2(arr, k);
 
         System.out.println("Longest Sub array is : " + result);
 
