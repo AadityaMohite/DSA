c1727e685dfc41efc5645b151ca351a1d798f679
Aaditya Mohite <mohiteaadi10@gmail.com>
2026-09-10 01:59:50 +0530

    Learning LinkedList

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+class Node{
+
+    int data ;
+    Node next;
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+};
+
+public class LinkedList{
+    public  static Node convertarrLL(int arr[]){
+
+          Node head = new Node(arr[0]);
+
+          Node mover = head;
+
+          for(int i = 1; i<arr.length; i++){
+             Node temp = new Node(arr[i]);
+             mover.next = temp;
+             mover = temp;
+          }
+
+
+        return head;
+    }
+
+    public static int checklength(Node head){
+     
+        int count = 0;
+        Node temp = head;
+
+        while(temp!=null){
+            temp = temp.next;
+            count++;
+        }
+
+        return count;
+    }
+
+
+   public static void main(String [] args){
+        // int arr[] = {5,6,10,12,16};
+
+         Scanner sc = new Scanner(System.in);
+
+         int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i =0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+      Node head  = convertarrLL(arr);
+
+      int count = checklength(head);
+
+       System.out.println(count);
+
+    Node temp = head;
+
+    while(temp!=null){
+        System.out.print(temp.data+" ");
+        temp = temp.next;
+    }
+
+   
+          sc.close();
+   }
+
+    
+}
---
+++ LinkedList/Searchelement.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+class Node{
+    int data ;
+    Node next;
+
+    
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+};
+public class Searchelement {
+
+    public static Node convertarrLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i = 1; i<arr.length; i++ ){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+
+        return head;
+    }
+    public static boolean Searchelement(Node head , int target){
+
+        Node temp = head;
+
+        while(temp!=null){
+            if(temp.data == target){
+                System.out.println(temp.data);
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+     int n = sc.nextInt();
+
+
+     int[] arr = new int[n];
+
+     for(int i = 0; i<arr.length; i++){
+        arr[i] = sc.nextInt();
+     }
+           
+     System.out.println("Enter the target Value to be searched : ");
+     int target = sc.nextInt();
+
+
+          Node head  = convertarrLL(arr);
+
+         boolean check =  Searchelement(head,target);
+
+         System.out.println(check);
+
+        sc.close();
+    }
+}
